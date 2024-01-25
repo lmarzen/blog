a088f824fb2bc4f8a00e5e51bee02a8f54fb9799
Luke Marzen <98473324+lmarzen@users.noreply.github.com>
2024-01-25 00:02:43 -0600

    remove unused css rules

--- scripts/gen-syntax.sh
+++ scripts/gen-syntax.sh
@@ -1,5 +1,11 @@
 hugo gen chromastyles --style=monokailight > chroma-light.css
 # hugo gen chromastyles --style=emacs > chroma-light.css
 hugo gen chromastyles --style=monokai > chroma-dark.css
-# hugo gen chromastyles --style=dracula > chroma-dark.css
+# hugo gen chromastyles --style=dracula > chroma-dark.cs
+
+# comment out empty rules
+sed -i -e 's|/\* \(.*\) \*/ \(\.chroma \.[a-zA-Z]*\) {  }|/* \1 */ /* \2 {  } */|g' chroma-light.css
+sed -i -e 's|/\* \(.*\) \*/ \(\.chroma \.[a-zA-Z]*\) {  }|/* \1 */ /* \2 {  } */|g' chroma-dark.css
+
+# make these rules apply only for dark mode
 sed -i 's/\.chroma/\.dark &/g' chroma-dark.css
